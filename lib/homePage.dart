d687fc60ecfa64a05e771e22bb9194697a0aa944
Vandan Kambodi <vandankambodi27@gmail.com>
2025-05-21 16:24:19 +0530

    Settings Page & Home Page design
    Implement Settings Page

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:snackzilla/settingPage.dart';
 
 class homePage extends StatefulWidget{
 
@@ -67,195 +68,281 @@ class _homePageState extends State<homePage> {
     },
   ];
 
+  List<Map<String, dynamic>> Rlist = [
+    {
+      'title': 'BBQ Burger',
+      'description': 'Cookie Heaven',
+      'price': '\$35.00',
+      'rating': 4.5,
+      'image': 'assets/Images/b2.png',
+      'review': 20,
+      'isVeg': false,
+    },
+    {
+      'title': 'BBQ Burger',
+      'description': 'Cookie Heaven',
+      'price': '\$35.00',
+      'rating': 4.5,
+      'image': 'assets/Images/b2.png',
+      'review': 20,
+      'isVeg': false,
+    },
+    {
+      'title': 'BBQ Burger',
+      'description': 'Cookie Heaven',
+      'price': '\$35.00',
+      'rating': 4.5,
+      'image': 'assets/Images/b2.png',
+      'review': 20,
+      'isVeg': false,
+    },
+    {
+      'title': 'BBQ Burger',
+      'description': 'Cookie Heaven',
+      'price': '\$35.00',
+      'rating': 4.5,
+      'image': 'assets/Images/b2.png',
+      'review': 20,
+      'isVeg': false,
+    },
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body:
-        Container(
+      appBar: PreferredSize(preferredSize: Size.fromHeight(123.0),
+      child: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent, // Make AppBar transparent to show your custom background
+        elevation: 0,
+        flexibleSpace: Container(
           color: Colors.white,
           child: Column(
             children: [
-              Container(
-                color: Colors.white,
-                child: Column(
-                  children: [
-                  Container(height: 30,
-                  decoration: BoxDecoration(
+              Container(height: 30,
+                decoration: BoxDecoration(
                     gradient: LinearGradient(colors: [Colors.red.shade400,Colors.red.shade400,])
-                  ),
-                  ),
-                    Container(
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(colors: [Colors.red.shade400,Colors.white,],
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter
-                        )
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 20.0,right: 20.0,top: 10.0,bottom: 10.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Icon(Icons.location_on,),
-                                SizedBox(width: 6,),
-                                Text("10 ABC Society,",style: TextStyle(fontSize: 14, fontFamily: 'description'),),
-                              ],
-                            ),
-                            Icon(Icons.notifications)
-                          ],
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10.0,right: 20.0,left: 20.0,bottom: 10.0),
-                      child: TextField(
-                        style: TextStyle(color: Colors.grey.shade800,fontFamily: 'description',fontSize: 14),
-                        cursorHeight: 20,
-                        cursorColor: Colors.black,
-                        decoration: InputDecoration(
-                          filled: true,
-                          contentPadding: EdgeInsets.symmetric(vertical: 12.0,),
-                          prefixIconColor: Colors.grey.shade800,
-                          suffixIconColor: Colors.grey.shade800,
-                          fillColor: Colors.grey.shade100,
-                          prefixIcon: Icon(Icons.search),
-                            suffixIcon: Icon(Icons.keyboard_voice_sharp),
-                            hintText: "Search..",
-                            hintStyle: TextStyle(color: Colors.grey.shade800, fontFamily: 'description', fontSize: 14),
-                            border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: BorderSide(color: Colors.white),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: BorderSide(color: Colors.white),
-                          )
-                        ),
-                      ),
-                    )
-                  ],
                 ),
               ),
-
-              Padding(
-                padding: const EdgeInsets.only(top: 12.0,bottom: 0.0,left: 5.0,right: 5.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.red.shade400,
-                  ),
-                  height: 170,
-                  width: 390,
+              Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [Colors.red.shade400,Colors.white,],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter
+                    )
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20.0,right: 20.0,top: 10.0,bottom: 10.0),
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                Text("25% ", style: TextStyle(color: Colors.white,fontSize: 30, fontStyle: FontStyle.normal, fontFamily: 'pageHead'),),
-                                Text("OFF",  style: TextStyle(color: Colors.white,fontSize: 30, fontStyle: FontStyle.normal,fontFamily: 'description'),),
-                              ],
-                            ),
-                            Text("IPL Special deal", style: TextStyle(color: Colors.grey.shade300,fontSize: 14, fontStyle: FontStyle.normal, fontFamily: 'pageHead'),),
-                            Text("Free Delivery", style: TextStyle(color: Colors.grey.shade300,fontSize: 10, fontStyle: FontStyle.normal, fontFamily: 'pageHead'),),
-                            SizedBox(height: 1,),
-                            ElevatedButton(onPressed: (){}, child: Text("Order now", style: TextStyle(
-                              color: Colors.grey.shade100,
-                              fontFamily: 'description',
-                              fontSize: 12,
-                            ),),style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.red.shade200,
-                              foregroundColor: Colors.white,
-                              minimumSize: Size(30, 35)
-                            ),)
-                          ],
-                        ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(Icons.location_on,),
+                          SizedBox(width: 6,),
+                          Text("10 ABC Society,",style: TextStyle(fontSize: 14, fontFamily: 'description'),),
+                        ],
                       ),
-                      Image.asset('assets/Images/b_main.png',height: 300,width: 209,)
+                      Icon(Icons.notifications)
                     ],
                   ),
                 ),
               ),
-
-              Container(
-                child:Text.rich(
-                  TextSpan(
-                    text: ".",
-                    style: TextStyle(fontSize: 25, color: Colors.grey),
-                    children: [
-                      TextSpan(
-                        text: ".",
-                        style: TextStyle(fontSize: 30, color: Colors.black),
-                        children: [
-                          TextSpan(
-                            text: ".",
-                            style: TextStyle(fontSize: 25, color: Colors.grey),
-                          )
-                        ]
-                      )
-                    ]
-                  )
-                )
-              ),
-
-              Container(
-                width: 390,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Top Picks", style: TextStyle(color: Colors.black, fontFamily: 'pageHead',fontSize: 16),),
-                    TextButton(onPressed: (){}, child: Text("View all", style: TextStyle(color: Colors.red.shade400, fontFamily: 'pageHead',fontSize: 12),),
-                      style: TextButton.styleFrom(foregroundColor: Colors.white),
-                    )
-                  ],
-                ),
-              ),
-
-              Container(
-                height: 305,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 16),
-                  itemCount: burgers.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.only(right: 12.0),
-                      child: BurgerCard(burgers[index]),
-                    );
-                  },
-                ),
-              ),
-
               Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
+                padding: const EdgeInsets.only(top: 10.0,right: 20.0,left: 20.0,bottom: 10.0),
+                child: TextField(
+                  style: TextStyle(color: Colors.grey.shade800,fontFamily: 'description',fontSize: 14),
+                  cursorHeight: 20,
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                      filled: true,
+                      contentPadding: EdgeInsets.symmetric(vertical: 12.0,),
+                      prefixIconColor: Colors.grey.shade800,
+                      suffixIconColor: Colors.grey.shade800,
+                      fillColor: Colors.grey.shade100,
+                      prefixIcon: Icon(Icons.search),
+                      suffixIcon: Icon(Icons.keyboard_voice_sharp),
+                      hintText: "Search..",
+                      hintStyle: TextStyle(color: Colors.grey.shade800, fontFamily: 'description', fontSize: 14),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide(color: Colors.white),
+                      )
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      ),
+      body:
+        SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Container(
+            color: Colors.white,
+            child: Column(
+              children: [
+
+                Padding(
+                  padding: const EdgeInsets.only(top: 12.0,bottom: 0.0,left: 5.0,right: 5.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.red.shade400,
+                    ),
+                    height: 170,
+                    width: 390,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Text("25% ", style: TextStyle(color: Colors.white,fontSize: 30, fontStyle: FontStyle.normal, fontFamily: 'pageHead'),),
+                                  Text("OFF",  style: TextStyle(color: Colors.white,fontSize: 30, fontStyle: FontStyle.normal,fontFamily: 'description'),),
+                                ],
+                              ),
+                              Text("IPL Special deal", style: TextStyle(color: Colors.grey.shade300,fontSize: 14, fontStyle: FontStyle.normal, fontFamily: 'pageHead'),),
+                              Text("Free Delivery", style: TextStyle(color: Colors.grey.shade300,fontSize: 10, fontStyle: FontStyle.normal, fontFamily: 'pageHead'),),
+                              SizedBox(height: 1,),
+                              ElevatedButton(onPressed: (){}, child: Text("Order now", style: TextStyle(
+                                color: Colors.grey.shade100,
+                                fontFamily: 'description',
+                                fontSize: 12,
+                              ),),style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.red.shade200,
+                                foregroundColor: Colors.white,
+                                minimumSize: Size(30, 35)
+                              ),)
+                            ],
+                          ),
+                        ),
+                        Image.asset('assets/Images/b_main.png',height: 300,width: 209,)
+                      ],
+                    ),
+                  ),
+                ),
+
+                Container(
+                  child:Text.rich(
+                    TextSpan(
+                      text: ".",
+                      style: TextStyle(fontSize: 25, color: Colors.grey),
+                      children: [
+                        TextSpan(
+                          text: ".",
+                          style: TextStyle(fontSize: 30, color: Colors.black),
+                          children: [
+                            TextSpan(
+                              text: ".",
+                              style: TextStyle(fontSize: 25, color: Colors.grey),
+                            )
+                          ]
+                        )
+                      ]
+                    )
+                  )
+                ),
+
+                Container(
                   width: 390,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text("Recommended", style: TextStyle(color: Colors.black, fontFamily: 'pageHead',fontSize: 16),),
+                      Text("Top Picks", style: TextStyle(color: Colors.black, fontFamily: 'pageHead',fontSize: 16),),
+                      TextButton(onPressed: (){}, child: Text("View all", style: TextStyle(color: Colors.red.shade400, fontFamily: 'pageHead',fontSize: 12),),
+                        style: TextButton.styleFrom(foregroundColor: Colors.white),
+                      )
                     ],
                   ),
                 ),
-              ),
 
-              
-            ],
+                Container(
+                  height: 305,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 16),
+                    itemCount: burgers.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 12.0),
+                        child: BurgerCard(burgers[index]),
+                      );
+                    },
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 390,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Recommended", style: TextStyle(color: Colors.black, fontFamily: 'pageHead',fontSize: 16),),
+                      ],
+                    ),
+                  ),
+                ),
+
+                Container(
+                  height: 305,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 16),
+                    itemCount: burgers.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 12.0),
+                        child: BurgerCard(burgers[index]),
+                      );
+                    },
+                  ),
+                ),
+
+
+              ],
+            ),
           ),
-        )
+        ),
+
+      bottomNavigationBar:
+      Container(
+        padding: EdgeInsets.symmetric(vertical: 12),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(top: BorderSide(color: Colors.grey.shade300)),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(onPressed: (){}, icon: Icon(Icons.home), color: Colors.red.shade400),
+            IconButton(onPressed: (){}, icon: Icon(Icons.restaurant_menu), color: Colors.grey),
+            IconButton(onPressed: (){}, icon: Icon(Icons.shopping_cart), color: Colors.grey),
+            IconButton(onPressed: (){}, icon: Icon(Icons.person), color: Colors.grey),
+            IconButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>settingPage()));
+            }, icon: Icon(Icons.settings), color: Colors.grey),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/settingPage.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:snackzilla/homePage.dart';
+
+class settingPage extends StatefulWidget{
+  @override
+  State<settingPage> createState() => _settingPageState();
+}
+
+class _settingPageState extends State<settingPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+
+      body:
+      Padding(
+        padding: const EdgeInsets.only(bottom: 12.0,left: 22.0,),
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Settings",style: TextStyle(color: Colors.black,fontFamily: 'title', fontSize: 20),),
+              SizedBox(height: 20,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Edit profile",style: TextStyle(color: Colors.black,fontFamily: 'pageHead', fontSize: 15),),
+                    Text("Change your name, description and profile photo",style: TextStyle(color: Colors.black,fontFamily: 'description', fontSize: 12),),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
+                child: Divider(
+                  color: Colors.grey.shade400,
+                  thickness: 0.5,
+                  indent: 0,
+                  endIndent: 20,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Notification settings",style: TextStyle(color: Colors.black,fontFamily: 'pageHead', fontSize: 15),),
+                    Text("Define what alerts and notifications you want to see",style: TextStyle(color: Colors.black,fontFamily: 'description', fontSize: 12),),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
+                child: Divider(
+                  color: Colors.grey.shade400,
+                  thickness: 0.5,
+                  indent: 0,
+                  endIndent: 20,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Account settings",style: TextStyle(color: Colors.black,fontFamily: 'pageHead', fontSize: 15),),
+                    Text("Delete your account.",style: TextStyle(color: Colors.black,fontFamily: 'description', fontSize: 12),),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
+                child: Divider(
+                  color: Colors.grey.shade400,
+                  thickness: 0.5,
+                  indent: 0,
+                  endIndent: 20,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Card details",style: TextStyle(color: Colors.black,fontFamily: 'pageHead', fontSize: 15),),
+                    Text("Change your UPI, Credit card and debit card details.",style: TextStyle(color: Colors.black,fontFamily: 'description', fontSize: 12),),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
+                child: Divider(
+                  color: Colors.grey.shade400,
+                  thickness: 0.5,
+                  indent: 0,
+                  endIndent: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+
+      backgroundColor: Colors.white,
+      bottomNavigationBar:
+      Container(
+        padding: EdgeInsets.symmetric(vertical: 12),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(top: BorderSide(color: Colors.grey.shade300)),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>homePage()));
+            }, icon: Icon(Icons.home), color: Colors.grey),
+            IconButton(onPressed: (){}, icon: Icon(Icons.restaurant_menu), color: Colors.grey),
+            IconButton(onPressed: (){}, icon: Icon(Icons.shopping_cart), color: Colors.grey),
+            IconButton(onPressed: (){}, icon: Icon(Icons.person), color: Colors.grey),
+            IconButton(onPressed: (){}, icon: Icon(Icons.settings), color: Colors.red.shade400),
+          ],
+        ),
+      ),
+    );
+  }
+}
