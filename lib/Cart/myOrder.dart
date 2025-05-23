af763c4eaa68837ea4ba19c799c1361c9322d47d
Vandan Kambodi <vandankambodi27@gmail.com>
2025-05-23 14:20:46 +0530

    Update myOrder.dart

--- lib/Cart/myOrder.dart
+++ lib/Cart/myOrder.dart
@@ -3,7 +3,6 @@ import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:snackzilla/menu/menuPage.dart';
 
-class myOrder extends StatefulWidget{
 class myOrder extends StatefulWidget {
   @override
   State<myOrder> createState() => _myOrderState();
